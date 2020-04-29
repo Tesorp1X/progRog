84cb599f90a6b2568b99d46ccb724fd875884c46
Tesorp1X <rakteam2014@gmail.com>
2020-04-30 05:39:00 +0700

    [MINOR UPDATE] Insertion fully debuged.
    DONE:
    +Balancing after insertion
    TODO:
    +Remove(Node)
    ++Balance
    +BST_free(root)
    +printLevel

--- prog1sem/lab4/redblactrees.c
+++ prog1sem/lab4/redblactrees.c
@@ -64,24 +64,56 @@ Node *Node_make(nodeColor color, char *word, Node *parent, Node *left, Node *rig
     return NULL;
 }
 
-void Node_rightRotate(Node **root, Node *newNode) {
+void Node_rightRotate(Node **root, Node *x) {
+    Node *y = x->left;
 
+    x->left = y->right;
+    if (y->right != NIL) y->right->parent = x;
+    if (x->parent) {
+        if (x == x->parent->right)
+            x->parent->right = y;
+        else 
+            x->parent->left = y;
+    } else 
+        *root = y;
+
+    y->right = x;
+    if (x != NIL)
+        x->parent = y;
 }
 
-void Node_leftRotate(Node **root, Node *newNode) {
+void Node_leftRotate(Node **root, Node *x) {
+    Node *y = x->right;
 
+    x->right = y->left;
+    if (y->left != NIL) y->left->parent = x;
+
+    if (y != NIL) y->parent = x->parent;
+    if (x->parent) {
+        if (x == x->parent->left)
+            x->parent->left = y;
+        else
+            x->parent->right = y;
+    } else 
+        *root = y;
+
+    y->left = x;
+    if (x != NIL) x->parent = y;
+        
 }
+
 
 /*Re-balancing RBT root with new node newNode. */
 void Node_fixInsertion(Node **root, Node *newNode) {
     /*While we have a violation*/
     while (newNode != *root && newNode->parent->color == RED) {
-        if (newNode->parent == newNode->parent->parent->right) {
-            Node *uncle = newNode->parent->parent->left;
+        if (newNode->parent == newNode->parent->parent->left) {
+            Node *uncle = newNode->parent->parent->right;
             if (uncle->color == RED) {
                 /*Uncle is RED*/
                 newNode->parent->color = BLACK;
                 uncle->color = BLACK;
+                newNode->parent->parent->color = RED;
                 newNode = newNode->parent->parent;
             } else {
                 /*Uncle id BLACK*/
@@ -96,7 +128,7 @@ void Node_fixInsertion(Node **root, Node *newNode) {
             }
         } else {
             /*Mirror situation*/
-            Node *uncle = newNode->parent->parent->right;
+            Node *uncle = newNode->parent->parent->left;
             if (uncle->color == RED) {
                 /* uncle is RED */
                 newNode->parent->color = BLACK;
@@ -117,23 +149,6 @@ void Node_fixInsertion(Node **root, Node *newNode) {
         }
     }
     (*root)->color = BLACK;
-}
-
-Node *Node_findParent(Node *root, Node *node) {
-    if (root->left == NULL && root->right == NULL)
-        return NULL;
-    if _compEQ(root->word, node->word)
-        return NULL;
-
-    if _compLT(root->word, node->word) {
-        if _compEQ(root->right->word, node->word) {
-            return root;
-        }
-    }else if _compLT(node->word, root->word) 
-        if _compEQ(root->left->word, node->word)
-            return root;
-
-    return _compLT(root->word, node->word) ? Node_find(root->right, node->word) : Node_find(root->left, node->word);
 }
 
 /*  Ads node with key-word 'word' and adds it to BST.   */
@@ -163,7 +178,7 @@ void Node_insert(Node **root, char *word) {
     } else
         *root = newNode;
     
-    //Node_fixInsertion(*root, newNode);
+    Node_fixInsertion(root, newNode);
 
 }
 
@@ -201,9 +216,9 @@ int main() {
     while (!_compEQ(line_buf, "DELETE:") && !feof(in)) {
         printf("Input: %s\t", line_buf);
         Node_insert(&root, line_buf);
-        printf("Root: %s\t", root->word);
-        printf("Node: %s\t", Node_find(root, line_buf)->word);
-        printf("Node color: %d\n", Node_find(root, line_buf)->color);
+        printf("Root: %s\n", root->word);
+        //printf("Node: %s\n", Node_find(root, line_buf)->word);
+       // printf("Node color: %d\n", Node_find(root, line_buf)->color);
         //if (Node_find(root, line_buf))
         fscanf(in, "%s", line_buf);
     }
