7453aec73f2d398321cf672639dfb98a05c1441a
Tesorp1X <rakteam2014@gmail.com>
2020-05-05 12:31:22 +0700

    [Struct-changes]

---
+++ jaba/HelloWorld/src/HelloWorld.java
@@ -0,0 +1,15 @@
+/**
+ * My first java program that prints <code>Hello, Sonya!</code>
+ *
+ * @author Artyom Gagin
+ * @version 1.0
+ */
+public class HelloWorld {
+    /**
+     * Program entry point.
+     * @param args  command-line arguments.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, Sonya!");
+    }
+}
