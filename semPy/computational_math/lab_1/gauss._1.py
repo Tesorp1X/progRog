8485f31c92defd7d146e96689b664494ebdc26ee
Artyom Gagin <rakteam2104@gmail.com>
2020-09-17 22:41:06 +0700

    New functionality: Now gauss_solve solves systems Ax = b. Returns vector x - solvation for the system.

--- semPy/computational_math/lab_1/gauss._1.py
+++ semPy/computational_math/lab_1/gauss._1.py
@@ -3,7 +3,7 @@
 
 # File inpur through input.txt #
 
-def read_matrix(isInt=True):
+def read_matrix(b, isInt=True):
 
     with open("input.txt") as file:
         n = int(file.readline())
@@ -13,10 +13,15 @@ def read_matrix(isInt=True):
             for i in range(n):
                 matr[i] = [int(t) for t in file.readline().split()]
         else:
-            matr = file.readlines()
+            for i in range(n):
+                matr[i] = float(file.readline.split())
+            '''matr = file.readlines()
             del matr[0]
-            matr = [[float(n) for n in x.split()] for x in matr]
-    
+            matr = [[float(n) for n in x.split()] for x in matr]'''
+
+        line = [float(t) for t in file.readline().split()]
+        for i in range(n):
+            b.append(line[i])
 
     print("Got matrix:")
     print_matrix(matr)
@@ -32,6 +37,9 @@ def print_matrix(matr):
             print(j, end="\t")
         print("\n")
         
+def print_vector(vector):
+    for num in vector:
+        print('|', num)
 
 def swap_lines(m, i, j):
     temp = m[i]
@@ -39,7 +47,7 @@ def swap_lines(m, i, j):
     m[j] = m[i]
 
 
-def solve_gauss(matr):
+def solve_gauss(matr, b):
     
     n = len(matr)
 
@@ -62,31 +70,57 @@ def solve_gauss(matr):
         else:
             break
 
-    return matr
+    x = [t for t in b]
+    if matr[n - 1][n - 1] != 0:
+        x[n - 1] /= matr[n - 1][n - 1]
+    i = n - 2
+
+    while i >= 0:
+        if matr[i][i] != 0:
+            sum = 0
+            for k in range(i + 1, n):
+                sum += matr[i][k] * x[k]
+            x[i] = (x[i] - sum) / matr[i][i]
+            i -= 1
+        else:
+            print("Система имеет бесконечное кол-во решений.")
+            break
+
+    return x
 
 
 
 
 
 if __name__ == "__main__":
+
+
     attempt = 0
     while attempt < 10:
-        is_int = input("Выбирете тип матрицы:\n1. Int\n2. float\n")
+
+        is_int = input("Выбирете тип матрицы:\n1. Int\n2. float\n3. Generate randomly.")
+        
         if is_int == '1':
             is_int = True
             break
         elif is_int == '2':
             is_int = False
             break
+        elif is_int == '3':
+            size = int(input("Введите размер: "))
+            low, high = float(input("Введите нижнюю и верхнюю границы для чисел: ").split())
+
         else:
             print("Error: wrong argument.")
             attempt += 1
 
     if attempt != 0:
         exit(1)
-
-    m = read_matrix(is_int)
-    solve_gauss(m)
-    print("\nResult matrix:")
-    print_matrix(m)
     
+    b = []
+    m = read_matrix(b, is_int)
+    x = solve_gauss(m, b)
+
+    print("\nResult vector:")
+    print_vector(x)
+    #print_matrix(m)
