c1f9636ab10c9f2c8eaad6bcfc12fa3117a94e4e
Artyom Gagin <rakteam2104@gmail.com>
2020-10-05 02:51:15 +0700

    To be reviewed...

--- c-cpp_labs/prog2sem/lab1/main.cpp
+++ c-cpp_labs/prog2sem/lab1/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <istream>
+#include <cstring>
 #define BUFFER_SIZE 16
 
 //TODO: Deal with utf-8 strings.
@@ -13,6 +14,15 @@ private:
 
     char* str;
 
+    String(unsigned int size, const char* str)
+            :  size(size), length(size), str(new char[size]) {
+        //TODO: utf-8 option
+
+        for (unsigned int i = 0; i < size; ++i) {
+            this->str[i] = str[i];
+        }
+    }
+
 
 public:
     /* Constructors */
@@ -21,32 +31,61 @@ public:
 
     explicit String(char symbol)
             : size(1), length(1), str(new char[size]) {
-
+            //TODO: utf-8 option
         str[0] = symbol;
 
     }
 
-    String(unsigned int length, const char* str)
-            :  size(length + 1), length(length), str(new char[size]) {
-            //TODO: constructor for line of given length
+
+    String(const char* str)
+            :  size(strlen(str)), length(strlen(str)), str(new char[strlen(str)]) {
+            //TODO: utf-8 option
+
+        for (unsigned int i = 0; i < this->size; ++i) {
+            this->str[i] = str[i];
+        }
     }
 
 
-    String(String& other)
+    String(const String& other)
             : size(other.size), length(other.length), str(new char[other.size]) {
-                //TODO: Copy constructor
+          //TODO: utf-8 option
+
+        for (unsigned int i = 0; i < size; ++i) {
+            this->str[i] = other.str[i];
+        }
+
     }
-
-
 
 
     ~String() {
         delete[] str;
     }
 
+
     /* Operators */
     String operator+(const String& other) {
-            //TODO: String concatenation via + operator
+        //TODO: String concatenation via + operator
+        String result_str;
+        result_str.str = new char[this->size + other.size];
+        result_str.size = this->size + other.size;
+        result_str.length = this->length + other.length;
+
+        for (int i = 0; i < this->size; ++i) {
+            result_str.str[i] = this->str[i];
+        }
+        for (int i = this->size; i < this->size + other.size; ++i) {
+            result_str.str[i] = other.str[i - this->size];
+        }
+
+        return {result_str.size, result_str.str};
+            
+    }
+
+    String operator+=(const String& string) {
+        String str = *this;
+        str = str + string;
+        return {str.size, str.str};
     }
 
     friend std::ostream& operator<< (std::ostream& output, String& string) {
@@ -57,6 +96,16 @@ public:
 
         return output;
     }
+
+    String operator* (unsigned int repeats) {
+        String string = *this;
+        for (unsigned int i = 1; i < repeats; ++i) {
+            string += string;
+        }
+
+        return {this->size * repeats, string.str};
+    }
+
 
     friend std::istream& operator>> (std::istream& input, String& string) {
 
@@ -72,7 +121,7 @@ public:
             if (line_length >= allocated_size) {
 
                 char* new_line = new char[allocated_size + BUFFER_SIZE];
-                for (int i = 0; i < line_length; ++i) {
+                for (unsigned int i = 0; i < line_length; ++i) {
                     new_line[i] = line[i];
                 }
 
@@ -94,7 +143,7 @@ public:
         string.size = line_length;
         delete [] string.str;
         string.str = new char[line_length];
-        for (int i = 0; i < line_length; ++i) {
+        for (unsigned int i = 0; i < line_length; ++i) {
             string.str[i] = line[i];
         }
 
@@ -103,7 +152,17 @@ public:
         return input;
     }
 
+
     //TODO: operator() (unsigned int left, unsigned int right)
+    String operator() (unsigned int first, unsigned int last) {
+        char* new_line = new char[last - first + 1];
+        for (unsigned int i = first; i < last + 1; ++i) {
+            new_line[i - first] = str[i];
+        }
+
+        return {last - first + 1, new_line};
+    }
+
 
 
     char operator[](unsigned int index){
@@ -121,12 +180,15 @@ public:
 
 int main() {
 
-    String str1('f');
-    std::cout << str1 << std::endl;
+    String str1("Hello world!");
+    //String str1('h');
+    String str2 = str1(6, 11);
+    //String str2 = str1;
+    String str3 = str2 * 3;
+    //str1 += str2;
+    std::cout << str1 << std::endl << str2 << std::endl << str3;
+    //std::cout << str1(6, 7) << std::endl << str2 << std::endl;
 
-    String str2;
-    std::cin >> str2;
-    std::cout << str2 << std::endl;
 
     return 0;
 }
