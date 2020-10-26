ae9f479936b7bd96fefa7614df39ef79718aeeb5
Tesorp1X <rakteam2014@gmail.com>
2020-10-26 14:59:50 +0700

    Using std::stable_sort() instead of buble sort.

--- c-cpp_labs/prog2sem/lab2/main.cpp
+++ c-cpp_labs/prog2sem/lab2/main.cpp
@@ -1,17 +1,11 @@
 #include <iostream>
 #include <string>
 #include <fstream>
-#include<algorithm>
+#include <algorithm>
 
 unsigned int countSetBits(unsigned int n)
 {
     unsigned int count = 0;
-    /*for(size_t i = 0; i < CHAR_BIT * sizeof n; ++i)
-    {
-        if ((n & (1 << i)) == 1)
-            ++count;
-    }
-*/
 
     while (n) {
         if (n % 2 == 1) ++count;
@@ -53,21 +47,14 @@ class Ascending : public Order {
 
 public:
     bool comparator(int a, int b) override {
-        return (a <= b);
+        return (a < b);
     }
 
     void sort(int* array, unsigned int size) override {
-        /*std::sort(array, array + size,
-                  [this](int x, int y) { return comparator(x, y); });*/
 
-        for (unsigned int i = 0; i < size - 1; ++i) {
-            for (unsigned int j = 0; j < size - i - 1; ++j) {
+        std::stable_sort(array, array + size,
+                  [this](int x, int y) { return comparator(x, y); });
 
-                if (!this->comparator(array[j], array[j + 1])) {
-                    swap(&array[j], &array[j + 1]);
-                }
-            }
-        }
     }
 
 };
@@ -76,18 +63,14 @@ class Descending : public Order {
 
 public:
     bool comparator(int a, int b) override {
-        return (a >= b);
+        return (a > b);
     }
 
     void sort(int* array, unsigned int size) override {
-        /*std::sort(array, array + size,
-                  [this](int x, int y) { return comparator(x, y); });*/
-        for (unsigned int i = 0; i < size - 1; ++i) {
-            for (unsigned int j = 0; j < size - i - 1; ++j) {
-                if (!this->comparator(array[j], array[j + 1]))
-                    swap(&array[j], &array[j + 1]);
-            }
-        }
+
+        std::stable_sort(array, array + size,
+                  [this](int x, int y) { return comparator(x, y); });
+
     }
 
 };
@@ -107,14 +90,10 @@ public:
     }
 
     void sort(int* array, unsigned int size) override {
-        /*std::sort(array, array + size,
-                  [this](int x, int y) { return comparator(x, y); });*/
-        for (unsigned int i = 0; i < size - 1; ++i) {
-            for (unsigned int j = 0; j < size - i - 1; ++j) {
-                if (!this->comparator(array[j], array[j + 1]))
-                    swap(&array[j], &array[j + 1]);
-            }
-        }
+
+        std::stable_sort(array, array + size,
+                  [this](int x, int y) { return comparator(x, y); });
+
     }
 };
 
@@ -129,32 +108,16 @@ public:
         : order(order) {}
 
     bool comparator(int a, int b) override {
-/*
-        unsigned int count_a = 0;
-        while (a) {
-            count_a += a & 1;
-            a >>= 1;
-        }
 
-        unsigned int count_b = 0;
-        while (b) {
-            count_b += b & 1;
-            b >>= 1;
-        }
-        */
         return order->comparator(countSetBits(a), countSetBits(b));
     }
 
 
     void sort(int* array, unsigned int size) override {
-        /*std::sort(array, array + size,
-                  [this](int x, int y) { return comparator(x, y); });*/
-        for (unsigned int i = 0; i < size - 1; ++i) {
-            for (unsigned int j = 0; j < size - i - 1; ++j) {
-                if (!this->comparator(array[j], array[j + 1]))
-                    swap(&array[j], &array[j + 1]);
-            }
-        }
+
+        std::stable_sort(array, array + size,
+                  [this](int x, int y) { return comparator(x, y); });
+
     }
 };
 
